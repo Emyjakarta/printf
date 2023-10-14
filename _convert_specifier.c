6d753f68dcded5af6fdaa5f574a6c0bd20bd19a4
lucky-blessed <blessednkwor7@gmail.com>
2023-10-14 23:14:28 +0100

    Variable modification

--- _convert_specifier.c
+++ _convert_specifier.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * int_conversion - Converts numbers to string.
- * @number: Numbers to be converted.
+ * @num: Numbers to be converted.
  */
-
-void int_conversion(int number)
+void int_conversion(int num)
 {
 	char _num_str[20];
 
-	num_to_str(number, _num_str);
+	num_to_str(num, _num_str);
 	write(1, _num_str, len_str(_num_str));
 }
