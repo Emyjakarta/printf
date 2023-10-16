418683be13d65f7a513c41056c571096cbdb2e52
Emyjakarta <ukonud4@gmail.com>
2023-10-16 01:20:59 +0100

    Final Update on task 0

--- _convert_specifier.c
+++ _convert_specifier.c
@@ -1,9 +1,13 @@
 #include "main.h"
-
+/**
+ * int_conversion-takes a number to be converted to string
+ * @number: number to be converted
+ * Return:converted number string
+ */
 int int_conversion(int number)
 {
 	char _num_str[20];
 
 	num_to_str(number, _num_str);
-	return (write(1, _num_str, len_str(_num_str)));
+	return (write(1, _num_str, strlen(_num_str)));
 }
--- _format_handlers.c
+++ _format_handlers.c
@@ -39,8 +39,39 @@ int percent_handler(va_list ptr)
 int d_handler(va_list ptr)
 {
 	int num = va_arg(ptr, int);
+	int str[10];
+	int Q, R, Add, length;
 
-	return (int_conversion(num));
+	length = 0;
+	R = 1000000000;
+	if (num < 0)
+	{
+		putchar('-');
+		length++;
+		num = -num;
+	}
+	if (num == 0)
+	{
+		putchar('0');
+		length++;
+		return (length);
+	}
+	str[0] = num / R;
+	for (Q = 1; Q < 10; Q++)
+	{
+		R /= 10;
+		str[Q] = (num / R) % 10;
+	}
+	for (Q = 0, Add = 0; Q < 10; Q++)
+	{
+		Add += str[Q];
+		if (Add != 0 || Q == 9)
+		{
+			putchar('0' + str[Q]);
+			length++;
+		}
+	}
+	return (length);
 }
 /**
  * get_handle_format - Checks the conversion specifiers
@@ -52,6 +83,7 @@ int d_handler(va_list ptr)
 int get_handle_format(const char *format, unsigned int *length, va_list ptr)
 {
 	unsigned int Q;
+	int num;
 	Handle_Format handle_format[] = {
 		{"c", c_handler},
 		{"s", s_handler},
@@ -65,7 +97,16 @@ int get_handle_format(const char *format, unsigned int *length, va_list ptr)
 	{
 		if (*format == *(handle_format[Q].specify))
 		{
-			*length += handle_format[Q].handler(ptr);
+			if (strcmp(handle_format[Q].specify, "d") == 0 ||
+					strcmp(handle_format[Q].specify, "i") == 0)
+			{
+				num = va_arg(ptr, int);
+				*length += int_conversion(num);
+			}
+			else
+			{
+				*length += handle_format[Q].handler(ptr);
+			}
 			return (1);
 		}
 	}
