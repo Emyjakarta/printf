9475052270fb0e018a5d5f964b0e3b1c643e8003
lucky-blessed <blessednkwor7@gmail.com>
2023-10-16 19:20:58 +0100

    format_handler2.c variables convention update

--- format_handler2.c
+++ format_handler2.c
@@ -1,60 +1,60 @@
 #include "main.h"
 /**
- * u_handler-handle unsigned integers
- * @ptr: variable argument list
- * Return:Length of the printed unsigned integer
+ * u_handler - Handle unsigned integers.
+ * @ptr: Variable argument list.
+ * Return: Length of the printed unsigned integer.
  */
 int u_handler(va_list ptr)
 {
-	unsigned int num = va_arg(ptr, unsigned int);
+	unsigned int number = va_arg(ptr, unsigned int);
 	char u_num_str[20];
 	int length;
 
-	if (num == 0)
+	if (number == 0)
 	{
 		u_num_str[0] = '0';
 		u_num_str[1] = '\0';
 		return (write(1, u_num_str, 1));
 	}
-	length = u_num_to_str(num, u_num_str);
+	length = u_num_to_str(number, u_num_str);
 	return (write(1, u_num_str, length));
 }
 /**
- * o_handler-handle octal integers
- * @ptr: variable argument list
- * Return:Length of the printed octal integer
+ * o_handler - Handle octal integers.
+ * @ptr: Variable argument list.
+ * Return: Length of the printed octal integer
  */
 int o_handler(va_list ptr)
 {
-	unsigned int num = va_arg(ptr, unsigned int);
+	unsigned int number = va_arg(ptr, unsigned int);
 	char num_str[20];
 
-	octal_to_str(num, num_str);
+	octal_to_str(number, num_str);
 	return (write(1, num_str, strlen(num_str)));
 }
 /**
- * x_handler-handle lowercase hexadecimal integers
- * @ptr: variable argument list
- * Return:Length of the printed lowercase hexadecimal integer
+ * x_handler - Handle lowercase hexadecimal integers
+ * @ptr: Variable argument list
+ * Return: Length of the printed lowercase hexadecimal integer
  */
 int x_handler(va_list ptr)
 {
-	unsigned int num = va_arg(ptr, unsigned int);
+	unsigned int number = va_arg(ptr, unsigned int);
 	char num_str[20];
 
-	hex_to_str(num, num_str, 0);
+	hex_to_str(number, num_str, 0);
 	return (write(1, num_str, strlen(num_str)));
 }
 /**
- * X_handler-handle uppercase hexadecimal integers
- * @ptr: variable argument list
- * Return:Length of the printed uppercase hexadecimal integer
+ * X_handler - Handle uppercase hexadecimal integers
+ * @ptr: Variable argument list
+ * Return: Length of the printed uppercase hexadecimal integer
  */
 int X_handler(va_list ptr)
 {
-	unsigned int num = va_arg(ptr, unsigned int);
+	unsigned int number = va_arg(ptr, unsigned int);
 	char num_str[20];
 
-	hex_to_str(num, num_str, 1);
+	hex_to_str(number, num_str, 1);
 	return (write(1, num_str, strlen(num_str)));
 }
